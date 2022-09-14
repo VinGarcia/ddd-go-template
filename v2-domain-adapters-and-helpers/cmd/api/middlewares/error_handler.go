087faa188e857b0c11f339e4c4c381910830ecbd
Vinícius Garcia <vingarcia00@gmail.com>
2022-09-14 12:13:45 -0300

    Refactor error translation logic from domain into the entrypoint adapter layer

--- v2-domain-adapters-and-helpers/cmd/api/middlewares/error_handler.go
+++ v2-domain-adapters-and-helpers/cmd/api/middlewares/error_handler.go
@@ -1,6 +1,9 @@
 package middlewares
 
 import (
+	"context"
+	"encoding/json"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/vingarcia/ddd-go-template/v2-domain-adapters-and-helpers/adapters/log"
 	"github.com/vingarcia/ddd-go-template/v2-domain-adapters-and-helpers/domain"
@@ -16,7 +19,7 @@ func HandleError(logger log.Provider) func(c *fiber.Ctx) error {
 		}
 
 		req := c.Request()
-		status, body := domain.HandleDomainErrAsHTTP(
+		status, body := handleDomainErrAsHTTP(
 			c.Context(),
 			logger,
 			err,
@@ -26,4 +29,42 @@ func HandleError(logger log.Provider) func(c *fiber.Ctx) error {
 		c.Status(status).Send(body)
 		return nil
 	}
+}
+
+func handleDomainErrAsHTTP(ctx context.Context, logger log.Provider, err error, method string, path string) (status int, responseBody []byte) {
+	domainErr := domain.AsDomainErr(err)
+
+	response := map[string]interface{}{
+		"code":       domainErr.Code,
+		"title":      domainErr.Title,
+		"request_id": domain.GetRequestIDFromContext(ctx),
+	}
+
+	switch domainErr.Code {
+	case "InternalErr":
+		status = 500
+
+		data := log.Body{
+			"route": method + ": " + path,
+		}
+		for k, v := range domainErr.Data {
+			data[k] = v
+		}
+		logger.Error(ctx, "request-error", data)
+
+	case "BadRequest":
+		status = 400
+		for k, v := range domainErr.Data {
+			response[k] = v
+		}
+
+	case "NotFoundErr":
+		status = 404
+		for k, v := range domainErr.Data {
+			response[k] = v
+		}
+	}
+
+	responseBody, _ = json.Marshal(response)
+	return status, responseBody
 }
