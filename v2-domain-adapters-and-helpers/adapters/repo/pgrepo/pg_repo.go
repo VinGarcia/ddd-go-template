ff17c4c3c90392ff748b3079940bc9b4668ff4a7
Vinícius Garcia <vingarcia00@gmail.com>
2023-11-28 20:33:51 -0300

    Simplify pg_repo.go

--- v2-domain-adapters-and-helpers/adapters/repo/pgrepo/pg_repo.go
+++ v2-domain-adapters-and-helpers/adapters/repo/pgrepo/pg_repo.go
@@ -9,41 +9,41 @@ import (
 	"github.com/vingarcia/ksql/adapters/kpgx"
 )
 
-// UsersRepo implements the repo.Users interface by using the ksql database.
-type UsersRepo struct {
+// Repo implements the repo.Users interface by using the ksql database.
+type Repo struct {
 	db ksql.Provider
 }
 
-// New instantiates a new UsersRepo
-func New(ctx context.Context, postgresURL string) (UsersRepo, error) {
+// New instantiates a new Repo
+func New(ctx context.Context, postgresURL string) (Repo, error) {
 	db, err := kpgx.New(ctx, postgresURL, ksql.Config{})
 	if err != nil {
-		return UsersRepo{}, domain.InternalErr("unable to start database", log.Body{
+		return Repo{}, domain.InternalErr("unable to start database", log.Body{
 			"error": err.Error(),
 		})
 	}
 
-	return UsersRepo{
+	return Repo{
 		db: db,
 	}, nil
 }
 
 // ChangeUserEmail implements the repo.Users interface
-func (u UsersRepo) ChangeUserEmail(ctx context.Context, userID int, newEmail string) error {
+func (u Repo) ChangeUserEmail(ctx context.Context, userID int, newEmail string) error {
 	return changeUserEmail(ctx, u.db, userID, newEmail)
 }
 
 // UpsertUser implements the repo.Users interface
-func (u UsersRepo) UpsertUser(ctx context.Context, user domain.User) (userID int, _ error) {
+func (u Repo) UpsertUser(ctx context.Context, user domain.User) (userID int, _ error) {
 	return upsertUser(ctx, u.db, user)
 }
 
 // GetUser implements the repo.Users interface
-func (u UsersRepo) GetUser(ctx context.Context, userID int) (domain.User, error) {
+func (u Repo) GetUser(ctx context.Context, userID int) (domain.User, error) {
 	return getUser(ctx, u.db, userID)
 }
 
 // GetUserByEmail implements the repo.Users interface
-func (u UsersRepo) GetUserByEmail(ctx context.Context, email string) (domain.User, error) {
+func (u Repo) GetUserByEmail(ctx context.Context, email string) (domain.User, error) {
 	return getUserByEmail(ctx, u.db, email)
 }
